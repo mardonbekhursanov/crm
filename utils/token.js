f257ac440262d47c881cb289dd43fe2c723539ae
mardonbekhursanov <mardonbekhursanov91@gmail.com>
2026-01-04 15:44:53 +0500

    access 7 kunlik refresh 30 kunlik qilindi

--- utils/token.js
+++ utils/token.js
@@ -2,9 +2,9 @@ const jwt = require("jsonwebtoken")
 
 
 const accessToken = (user) =>
-  jwt.sign({ id: user.id, role: user.role }, process.env.SECRETKEY, { expiresIn: "15m" });
+  jwt.sign({ id: user.id, role: user.role }, process.env.SECRETKEY, { expiresIn: "7d" });
 
 const refreshToken = (user) =>
-  jwt.sign({ id: user.id }, process.env.REFRESHKEY, { expiresIn: "7d" });
+  jwt.sign({ id: user.id }, process.env.REFRESHKEY, { expiresIn: "30d" });
 
 module.exports = { accessToken, refreshToken };
