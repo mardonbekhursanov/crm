709c1335f70e892e7f44ccb2ae35f0f5ae9be22f
mardonbekhursanov <mardonbekhursanov91@gmail.com>
2026-01-05 14:54:21 +0500

    SSL qo'shildi

--- models/index.js
+++ models/index.js
@@ -3,6 +3,12 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // ⚠️ MUHIM
+    },
+  },
 });
 
 const db = {};
